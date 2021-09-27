31d00e089fb898c1c75d5709a2492bf1b4caa972
TusharPagadalaAshok <pagadala.t@northeastern.edu>
2021-09-26 21:24:53 -0400

    Lab Assignment one

---
+++ LabAssignment_!/src/model/Product.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author patus
+ */
+public class Product {
+    private String Name;
+    private String GeoData;
+    private String DOB;
+    private long Phoneno;
+    private long faxno;
+    private String emailaddr;
+    private long SSN;
+    private long MedicalRecordno;
+    private long Healthplanbeneficiaryno;
+    private long Bankaccno;
+    private long certno;
+    private long Vechilelicenseplate;
+    private long Deviceidentifier;
+    private String Linkedin;
+    private String IPaddr;
+    private String Image;
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String Image) {
+        this.Image = Image;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getGeoData() {
+        return GeoData;
+    }
+
+    public void setGeoData(String GeoData) {
+        this.GeoData = GeoData;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public long getPhoneno() {
+        return Phoneno;
+    }
+
+    public void setPhoneno(long Phoneno) {
+        this.Phoneno = Phoneno;
+    }
+
+    public long getFaxno() {
+        return faxno;
+    }
+
+    public void setFaxno(long faxno) {
+        this.faxno = faxno;
+    }
+
+    public String getEmailaddr() {
+        return emailaddr;
+    }
+
+    public void setEmailaddr(String emailaddr) {
+        this.emailaddr = emailaddr;
+    }
+
+    public long getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(long SSN) {
+        this.SSN = SSN;
+    }
+
+    public long getMedicalRecordno() {
+        return MedicalRecordno;
+    }
+
+    public void setMedicalRecordno(long MedicalRecordno) {
+        this.MedicalRecordno = MedicalRecordno;
+    }
+
+    public long getHealthplanbeneficiaryno() {
+        return Healthplanbeneficiaryno;
+    }
+
+    public void setHealthplanbeneficiaryno(long Healthplanbeneficiaryno) {
+        this.Healthplanbeneficiaryno = Healthplanbeneficiaryno;
+    }
+
+    public long getBankaccno() {
+        return Bankaccno;
+    }
+
+    public void setBankaccno(long Bankaccno) {
+        this.Bankaccno = Bankaccno;
+    }
+
+    public long getCertno() {
+        return certno;
+    }
+
+    public void setCertno(long certno) {
+        this.certno = certno;
+    }
+
+    public long getVechilelicenseplate() {
+        return Vechilelicenseplate;
+    }
+
+    public void setVechilelicenseplate(long Vechilelicenseplate) {
+        this.Vechilelicenseplate = Vechilelicenseplate;
+    }
+
+    public long getDeviceidentifier() {
+        return Deviceidentifier;
+    }
+
+    public void setDeviceidentifier(long Deviceidentifier) {
+        this.Deviceidentifier = Deviceidentifier;
+    }
+
+    public String getLinkedin() {
+        return Linkedin;
+    }
+
+    public void setLinkedin(String Linkedin) {
+        this.Linkedin = Linkedin;
+    }
+
+    public String getIPaddr() {
+        return IPaddr;
+    }
+
+    public void setIPaddr(String IPaddr) {
+        this.IPaddr = IPaddr;
+    }
+  
+    
+    
+    
+}
