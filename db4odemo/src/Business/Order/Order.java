aa3e046c8715a0251436f3af5595a707491af594
TusharPagadalaAshok <pagadala.t@northeastern.edu>
2021-11-16 13:28:54 -0500

    Commit - skeleton

---
+++ db4odemo/src/Business/Order/Order.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Order;
+
+/**
+ *
+ * @author patus
+ */
+public class Order {
+    private String orderId;
+    private String restaurantName;
+    private String customerName;
+    private String deliveryMan;
+    private String orderCost;
+    private String orderStatus;
+    private String deliveryAddress;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getDeliveryMan() {
+        return deliveryMan;
+    }
+
+    public void setDeliveryMan(String deliveryMan) {
+        this.deliveryMan = deliveryMan;
+    }
+
+    public String getOrderCost() {
+        return orderCost;
+    }
+
+    public void setOrderCost(String orderCost) {
+        this.orderCost = orderCost;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+    
+    @Override
+    public String toString() {
+        return orderId;
+    }
+    
+    
+}
